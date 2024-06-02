201165566d78642d744814a4c78051e58656c947
Franko Anticevic <franko.anticevic@foreo.com>
2024-06-02 12:02:59 +0200

    preact and component island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://ubiquitous-begonia-1193d3.netlify.app"
+  site: "https://ubiquitous-begonia-1193d3.netlify.app",
+  integrations: [preact()]
 });
